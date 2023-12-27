344d285493db3f278639dafd271d83bb96d97320
Xtemper <92599384+itsxtemper@users.noreply.github.com>
2023-12-27 19:41:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Yay, see you on the tonight! Baby Girl";
+  question.innerHTML = "Nooooobbbbbbb ";
   gif.src =
     "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 });
