db76acaec4fa55c01f9899ba5ec4361ff6375849
Xtemper <92599384+itsxtemper@users.noreply.github.com>
2024-02-18 22:35:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Mystery";
+  question.innerHTML = "See you tonight baby girl";
   gif.src =
     "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 });
