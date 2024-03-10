7c4ac8f09a84b07abd9a2596520de7f1db22aa64
Harsh <harshprakash06@outlook.com>
2024-03-11 04:27:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "See you tonight baby girl";
+  question.innerHTML = "Give me your insta Babygirl";
   gif.src =
     "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 });
