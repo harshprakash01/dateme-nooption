2161c61646fe9a2d85e824fdb86464c2d3cd4e50
Harsh <harshprakash06@outlook.com>
2024-03-13 01:56:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Get naked my Babygirl";
+  question.innerHTML = "Love you Babygirl";
   gif.src =
     "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 });
